802aac1ddf0f7f0184eb44095152fb4d6bbc3a4c
Sam Goldsmith <samagold@Sams-MacBook-Pro.local>
2020-08-04 11:27:19 -0600

    Updates formatting

--- test/app.js
+++ test/app.js
@@ -5,7 +5,7 @@ import perfect from '../app.js';
 const { expect } = chai;
 
 describe('#perfect', () => {
-	it('should log to the console', () => {
-		expect(perfect()).to.equal('Not perfect yet.');
-	});
-})
+  it('should log to the console', () => {
+    expect(perfect()).to.equal('Not perfect yet.');
+  });
+});
